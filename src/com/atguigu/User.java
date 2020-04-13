a70f7d9760025bf0130afe014ca2fab273346956
杨小麟 <1669769092@qq.com>
2020-04-13 20:06:17 +0800

    1111

--- src/com/atguigu/User.java
+++ src/com/atguigu/User.java
@@ -7,5 +7,9 @@ package com.atguigu;
 public class User {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
     }
 }
