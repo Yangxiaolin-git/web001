b2d34bb9a1b82309f8fe2cfb6ec7120ec3c5f9ce
杨小麟 <1669769092@qq.com>
2020-04-13 20:01:54 +0800

    Initial commit

---
+++ src/com/atguigu/User.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author Administrator
+ * @create 2020-04-13 19:49
+ */
+public class User {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
